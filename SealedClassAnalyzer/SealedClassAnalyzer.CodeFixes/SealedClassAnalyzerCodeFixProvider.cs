7f5b4d82c89cf28e76b57aa1cc8a5ed980cc36f8
Egor <sychov1991@mail.ru>
2024-11-28 12:29:58 +0500

    Prepared for publish

--- SealedClassAnalyzer/SealedClassAnalyzer.CodeFixes/SealedClassAnalyzerCodeFixProvider.cs
+++ SealedClassAnalyzer/SealedClassAnalyzer.CodeFixes/SealedClassAnalyzerCodeFixProvider.cs
@@ -4,6 +4,7 @@ using Microsoft.CodeAnalysis.CodeFixes;
 using Microsoft.CodeAnalysis.CSharp;
 using Microsoft.CodeAnalysis.CSharp.Syntax;
 using Microsoft.CodeAnalysis.Rename;
+using Microsoft.CodeAnalysis.Text;
 using System.Collections.Immutable;
 using System.Composition;
 using System.Linq;
@@ -15,6 +16,8 @@ namespace SealedClassAnalyzer
     [ExportCodeFixProvider(LanguageNames.CSharp, Name = nameof(SealedClassAnalyzerCodeFixProvider)), Shared]
     public class SealedClassAnalyzerCodeFixProvider : CodeFixProvider
     {
+        private const string title = "Добавить модификатор 'sealed'";
+
         public sealed override ImmutableArray<string> FixableDiagnosticIds
         {
             get { return ImmutableArray.Create(SealedClassAnalyzer.DiagnosticId); }
@@ -28,41 +31,43 @@ namespace SealedClassAnalyzer
 
         public sealed override async Task RegisterCodeFixesAsync(CodeFixContext context)
         {
-            var root = await context.Document.GetSyntaxRootAsync(context.CancellationToken).ConfigureAwait(false);
-
-            // TODO: Replace the following code with your own analysis, generating a CodeAction for each fix to suggest
-            var diagnostic = context.Diagnostics.First();
+            // Получаем первое диагностическое сообщение
+            var diagnostic = context.Diagnostics[0];
             var diagnosticSpan = diagnostic.Location.SourceSpan;
 
-            // Find the type declaration identified by the diagnostic.
-            var declaration = root.FindToken(diagnosticSpan.Start).Parent.AncestorsAndSelf().OfType<TypeDeclarationSyntax>().First();
-
-            // Register a code action that will invoke the fix.
+            // Регистрируем кодовое действие для предоставления исправления
             context.RegisterCodeFix(
                 CodeAction.Create(
-                    title: CodeFixResources.CodeFixTitle,
-                    createChangedSolution: c => MakeUppercaseAsync(context.Document, declaration, c),
-                    equivalenceKey: nameof(CodeFixResources.CodeFixTitle)),
+                    title: title,
+                    createChangedDocument: c => AddSealedModifierAsync(context.Document, diagnosticSpan, c),
+                    equivalenceKey: title),
                 diagnostic);
         }
 
-        private async Task<Solution> MakeUppercaseAsync(Document document, TypeDeclarationSyntax typeDecl, CancellationToken cancellationToken)
+        private async Task<Document> AddSealedModifierAsync(Document document, TextSpan diagnosticSpan, CancellationToken cancellationToken)
         {
-            // Compute new uppercase name.
-            var identifierToken = typeDecl.Identifier;
-            var newName = identifierToken.Text.ToUpperInvariant();
+            // Получаем синтаксическое дерево и корневой узел
+            var syntaxRoot = await document.GetSyntaxRootAsync(cancellationToken).ConfigureAwait(false);
 
-            // Get the symbol representing the type to be renamed.
-            var semanticModel = await document.GetSemanticModelAsync(cancellationToken);
-            var typeSymbol = semanticModel.GetDeclaredSymbol(typeDecl, cancellationToken);
+            // Находим узел класса по позиции диагностического сообщения
+            var classDeclaration = syntaxRoot.FindToken(diagnosticSpan.Start).Parent.AncestorsAndSelf().OfType<ClassDeclarationSyntax>().First();
 
-            // Produce a new solution that has all references to that type renamed, including the declaration.
-            var originalSolution = document.Project.Solution;
-            var optionSet = originalSolution.Workspace.Options;
-            var newSolution = await Renamer.RenameSymbolAsync(document.Project.Solution, typeSymbol, newName, optionSet, cancellationToken).ConfigureAwait(false);
+            // Проверяем, есть ли уже модификатор 'sealed' (на всякий случай)
+            if (classDeclaration.Modifiers.Any(SyntaxKind.SealedKeyword))
+                return document;
 
-            // Return the new solution with the now-uppercase type name.
-            return newSolution;
+            // Добавляем модификатор 'sealed'
+            var sealedModifier = SyntaxFactory.Token(SyntaxKind.SealedKeyword).WithTrailingTrivia(SyntaxFactory.Space);
+            var newModifiers = classDeclaration.Modifiers.Add(sealedModifier);
+
+            // Создаем новый узел класса с обновленными модификаторами
+            var newClassDeclaration = classDeclaration.WithModifiers(newModifiers);
+
+            // Обновляем синтаксическое дерево
+            var newSyntaxRoot = syntaxRoot.ReplaceNode(classDeclaration, newClassDeclaration);
+
+            // Возвращаем обновленный документ
+            return document.WithSyntaxRoot(newSyntaxRoot);
         }
     }
 }
--- SealedClassAnalyzer/SealedClassAnalyzer.Test/SealedClassAnalyzerUnitTests.cs
+++ SealedClassAnalyzer/SealedClassAnalyzer.Test/SealedClassAnalyzerUnitTests.cs
@@ -1,4 +1,29 @@
-﻿namespace SealedClassAnalyzer.Test
-{
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Microsoft;
+using VerifyCS = SealedClassAnalyzer.Test.CSharpCodeFixVerifier<
+    SealedClassAnalyzer.SealedClassAnalyzer,
+    Microsoft.CodeAnalysis.Testing.EmptyCodeFixProvider>;
+using System.Threading.Tasks;
 
+namespace SealedClassAnalyzer.Test
+{
+    [TestClass]
+    public class SealedClassAnalyzerUnitTests
+    {
+        [TestMethod]
+        public async Task TestClassNotSealed()
+        {
+            var test = @"
+            namespace TestNamespace
+            {
+                class TestClass
+                {
+                }
+            }"
+            ;
+
+            var expected = VerifyCS.Diagnostic("SCA001").WithLocation(4, 23).WithArguments("TestClass");
+            await VerifyCS.VerifyAnalyzerAsync(test, expected);
+        }
+    }
 }
